6a951dfdcb72ba2cf53d5c6bf5ac01dd30313822
unknown <gracebrightness@gmail.com>
2022-11-10 11:47:30 +0300

    laravel second

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('page-content')
+       HOME PAGE
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+//pass variable
+//Route::get('/user/{id}/{name}', function($id,$name){
+ //   return 'This is my name'.$name.'And this is my id'.$id;
+//});
