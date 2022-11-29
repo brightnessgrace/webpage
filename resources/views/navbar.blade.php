d89fff72ec453ea63ce2204c07c8387ca2d9ebc8
unknown <gracebrightness@gmail.com>
2022-11-29 15:38:13 +0300

    another update

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -9,19 +9,97 @@
 </head>
 <body>
     <div class="nav-bar-container">
-        <ul class="menu-nav">
+        <!--ul class="menu-nav">
         
-                        <li><a class="act">Mwanzo</a></li>
-                        <li><a class="act">Kuhusu PSSSF</a></li>
-                        <li><a class="act">Mafao Yetu</a></li>
-                        <li><a class="act">Uwekezaji</a></li>
-                        <li><a class="act">e Marejesho</a></li>
-                        <li><a class="act">Habari Zetu</a></li>
-                        <li><a class="act">Maulizo</a></li>
+                        <div class="subnav"><li><a class="act">Mwanzo</a></li></div>
+                        <div class="subnav">
+                        <li><a class="act">Kuhusu PSSSF <i class="fa fa-caret-down"></i></a></li>
+                            <div class="subnav-content">
+                            <a href="#company">Profili</a>
+                            <a href="#team">Bodi</a>
+                            <a href="#careers">Management</a>
+                            <a href="#careers">Salamu za Mkurugenzi</a>
+                            </div>
+                        </div>
+                        <div class="subnav">
+                        <li><a class="act">Mafao Yetu 
+                              <i class="fa fa-caret-down"></i>
+                        </a></li>
+                            <div class="subnav-content">
+                                <a href="#company">Fao La Uzee</a>
+                                <a href="#team">Fao La Uzazi</a>
+                                <a href="#careers">Fao La Kukosa Kazi</a>
+                                <a href="#careers">Fao La Kifo</a>
+                                <a href="#careers">Fao La Kuumia Kazini</a>
+                            </div>
+                       </div>
+                        <li><a class="act">Uwekezaji<i class="fa fa-caret-down"></i></a></li>
+                        <li><a class="act">e Marejesho<i class="fa fa-caret-down"></i></a></li>
+                        <li><a class="act">Habari Zetu<i class="fa fa-caret-down"></i></a></li>
+                        <li><a class="act">Maulizo<i class="fa fa-caret-down"></i></a></li>
                         
 
                     
-            </ul>
+            </ul-->
+
+            <div class="navbar">
+            <a href="#home">Mwanzo</a>
+            <div class="subnav">
+                <button class="subnavbtn">Kuhusu Sisi <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                <a href="#company">Company</a>
+                <a href="#team">Team</a>
+                <a href="#careers">Careers</a>
+                </div>
+            </div> 
+            <div class="subnav">
+                <button class="subnavbtn">Mafao Yetu <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                <a href="#bring">Bring</a>
+                <a href="#deliver">Deliver</a>
+                <a href="#package">Package</a>
+                <a href="#express">Express</a>
+                </div>
+            </div> 
+            <div class="subnav">
+                <button class="subnavbtn">Uwekezaji <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                <a href="#link1">Link 1</a>
+                <a href="#link2">Link 2</a>
+                <a href="#link3">Link 3</a>
+                <a href="#link4">Link 4</a>
+                </div>
+            </div>
+            <div class="subnav">
+                <button class="subnavbtn">Habari <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                <a href="#link1">Link 1</a>
+                <a href="#link2">Link 2</a>
+                <a href="#link3">Link 3</a>
+                <a href="#link4">Link 4</a>
+                </div>
+            </div>
+            <div class="subnav">
+                <button class="subnavbtn">e Mrejesho <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                <a href="#link1">Link 1</a>
+                <a href="#link2">Link 2</a>
+                <a href="#link3">Link 3</a>
+                <a href="#link4">Link 4</a>
+                </div>
+            </div>
+            <div class="subnav">
+                <button class="subnavbtn">Maulizo <i class="fa fa-caret-down"></i></button>
+                <div class="subnav-content">
+                <a href="#link1">Link 1</a>
+                <a href="#link2">Link 2</a>
+                <a href="#link3">Link 3</a>
+                <a href="#link4">Link 4</a>
+                </div>
+            </div>
+            
+            </div>
+
     </div>
 </body>
 </html>
